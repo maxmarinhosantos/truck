267fbefe54d38ba36d9377bad55a510209063cb8
Pierre-Yves <pierre-yves.monnet@bonitasoft.com>
2019-07-03 15:47:48 -0700

    Accept the parameters as a String

--- GroovyMaintenance/CancelProcessInstance.groovy
+++ GroovyMaintenance/CancelProcessInstance.groovy
@@ -1,10 +1,16 @@
 
 import org.bonitasoft.engine.api.ProcessAPI ;
 
-Long processInstance = {{processInstanceId;tips:This process instance will be cancelled (archived)}}
+
 ProcessAPI processAPI = apiAccessor.getProcessAPI();
 try
 {
+   String processInstanceSt = {{processInstanceId;tips:This process instance will be cancelled (archived)}}
+   if (processInstanceSt ===null)
+   {
+      return "Please give a processInstanceId";
+   }
+   long processInstance = Long.valueOf( processInstanceSt );
    processAPI.cancelProcessInstance( processInstance ) ;
    return "Case "+processInstance+" was successfully cancelled (archived)";
 }
